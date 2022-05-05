efc0f2413d644a796f19544b23aa6adc2a371b60
MKazu <mkazuhisa22144@gmail.com>
2022-05-06 02:25:27 +0900

    test2

--- WpfApp1/MainWindow.xaml.vb
+++ WpfApp1/MainWindow.xaml.vb
@@ -2,16 +2,11 @@
 Class MainWindow
     Dim ymdS As Date
     Private Sub Button1_Click(sender As Object, e As RoutedEventArgs)
-        Label2.DataContext = Format(Now, "yyyy年mm月dd日")
 
-        'ymdS = Format(Now, "yyyy年mm月dd日")
-        Label1.DataContext = 1
-
-        Label1.DataContext = Label1.DataContext + 2
 
     End Sub
 
     Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
-        testText.Text = "Hello! .NET"
+        testText.Text = Format(Now, "yyyy年MM月dd日")
     End Sub
 End Class
